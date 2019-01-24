6a5d2ba88d7a75c8d73c8b67f8f0b1483e369fba
秦嘉鹏 <qinjiapeng@3lengjing.com>
2019-01-24 20:43:34 +0800

    优化百度编辑器

--- src/components/Editors/Ueditor.js
+++ src/components/Editors/Ueditor.js
@@ -1,37 +1,46 @@
 import React, { Component } from 'react';
+import uuidv1 from 'uuid/v1';
 
+// eslint-disable-next-line no-underscore-dangle
 const { uploadHost } = window._config;
-const UE = window.UE;
+const { UE } = window;
 
-class Ueditor extends Component {
-  componentDidMount() {
-    this.UEditor = UE.getEditor(this.box, {
-      autoClearinitialContent: true, //focus时自动清空初始化时的内容
-      wordCount: false, //关闭字数统计
-      elementPathEnabled: false, //关闭elementPath
-      serverUrl: uploadHost,
-      initialFrameWidth: '100%',
-      initialFrameHeight: '600',
-    });
-    this.UEditor.ready(() => {
-      const { value } = this.props;
-      this.UEditor.setContent(value || '');
-      this.UEditor.addListener('selectionchange', this.onChange);
-    });
-  }
-  componentWillUnmount() {
-    this.UEditor.removeListener('selectionchange', this.onChange);
-    UE.delEditor(this.box);
-    this.box.parentElement.removeChild(this.box);
-  }
-  onChange = () => {
-    const { onChange } = this.props;
-    const value = this.UEditor.getContent();
-    if (onChange) onChange(value);
-  };
-  render() {
-    return <div ref={ref => (this.box = ref)} />;
-  }
+export default class Ueditor extends Component {
+    constructor(props){
+        super(props);
+        this.id = uuidv1();
+    }
+
+    componentDidMount() {
+        this.UEditor = UE.getEditor(this.id, {
+            autoClearinitialContent: true, // focus时自动清空初始化时的内容
+            wordCount: false, // 关闭字数统计
+            elementPathEnabled: false, // 关闭elementPath
+            serverUrl: uploadHost,
+            initialFrameWidth: '100%',
+            initialFrameHeight: '600',
+        });
+        this.UEditor.ready(() => {
+            const { value } = this.props;
+            this.UEditor.setContent(value || '');
+            this.UEditor.addListener('selectionchange', this.onChange);
+        });
+    }
+
+    componentWillUnmount() {
+        this.UEditor.removeListener('selectionchange', this.onChange);
+        UE.delEditor(this.id);
+        const elem = document.getElementById(this.id);
+        elem.parentElement.removeChild(elem);
+    }
+
+    onChange = () => {
+        const { onChange } = this.props;
+        const value = this.UEditor.getContent();
+        if (onChange) onChange(value);
+    };
+
+    render() {
+        return (<div> <div id={this.id} /> </div>);
+    }
 }
-
-export default Ueditor;
